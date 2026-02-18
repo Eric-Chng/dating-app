9eafdf9ab68b1fa55839781b6bae75ebfb43ff92
Eric Cheng <eric.bc8@gmail.com>
2026-02-18 14:30:59 -0500

    ids

--- profiles.js
+++ profiles.js
@@ -1,30 +1,30 @@
 export const PROFILES = [
   {
-    id: "me-001",
-    name: "train",
+    id: "me-1",
+    name: "Eric",
     bio: "hi im eric",
     photo: "./images/train.jpg",
   },
   {
-    id: "me-002",
-    name: "griffin_walnut",
+    id: "cat-2",
+    name: "Griffin with a Walnut",
     bio: "i miss my cat",
     photo: "./images/griffin_walnut.jpg",
   },
   {
-    id: "me-003",
+    id: "me-2",
     name: "digi",
     bio: "i like this sweater",
     photo: "./images/digi.jpg",
   },
   {
-    id: "me-004",
+    id: "friends-1",
     name: "friends.jpg",
     bio: "here's a nice photo of some of my friends",
     photo: "./images/friends.jpg",
   },
   {
-    id: "me-005",
+    id: "me-3",
     name: "pizza.jpg",
     bio: "i love food man",
     photo: "./images/pizza.jpg",
